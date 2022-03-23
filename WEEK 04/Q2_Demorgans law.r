a3e08938831773115e08f635e1a624487bde9d16
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-23 11:52:52 +0530

    update

---
+++ WEEK 04/Q2_Demorgans law.r
@@ -0,0 +1,19 @@
+library(FuzzyR)
+library(sets)
+library(ggplot2)
+df <- data.frame(x= c(1,2,3,4,5,6))
+A <- c(0.1, 0.2, 0.7, 1, 0.6, 0.4)
+B <- c(0.3, 0.8, 0.2, 0.6, 1, 0.9)
+fuzzy_logic("Zadeh")
+#De-Morgan's laws
+#Right Hand Side 1st law
+.T.(A,B)
+#Left Hand Side 1st law
+.N.(.S.(.N.(A), .N.(B)))
+#First Law proved
+
+#Right Hand Side 2st law
+.S.(A,B)
+#Left Hand Side 2st law
+.N.(.T.(.N.(A), .N.(B)))
+#Second law proved
