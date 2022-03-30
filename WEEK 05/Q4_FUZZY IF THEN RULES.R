37f2720cff9783076924f154719c361e3ec73306
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-30 12:46:42 +0530

    update

--- WEEK 05/Q4_FUZZY IF THEN RULES.R
+++ WEEK 05/Q4_FUZZY IF THEN RULES.R
@@ -5,22 +5,25 @@ library(sets)
 # B*= Qualified consequent(Fuzzy Set on Y)
 # R = A->B = Fuzzy Rule on XxY, where  A is anticident and B is consequent
 
-A_ <- c(0.1, 0.3, 0.7, 0.8)
+mf0<- c(0.1, 0.3, 0.7, 0.8)
+A_ <- matrix(mf1, nrow=4, ncol=3, byrow=TRUE)
+print(A_)
+
 mf1 <- c(0.38, 0.8, 0.6, 0.2, 0.4, 0.9, 1, 0.8, 0.7, 0.6, 0.2, 0.26)
 R <- matrix(mf1, nrow=4, ncol=3, byrow=TRUE)
 print(R)
 
 r1 <- 1:4
 r2 <- 1:2
+c1 <- 1:3
+
 B_ <- matrix(c(0,0,0,0,0,0,0,0,0,0,0,0), nrow=4,ncol=3, byrow=TRUE)
 print(B_)
-for(i in r1)
-{
-  for (j in r2)
-  {
-    for (k in c1)
-    {
-      R3[i,j]<-max(R3[i,j], R1[i,k]*R2[k,j])
+for (i in r1){
+  for (j in r2){
+    for (k in  c1){
+      B_[i,j] <- max(B_[i,j], min(A_[i,k], R[k,j]))
     }
   }
 }
+print(B_)
