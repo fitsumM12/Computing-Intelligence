d4e24bf9d3a9aed6de39dcfbcff8548fa5cce91f
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-30 11:32:28 +0530

    update

--- WEEK 05/matrix multiplication in r.r
+++ WEEK 05/matrix multiplication in r.r
@@ -1,3 +1,4 @@
+#MATRIX MULTIPLICATION USING R
 library(sets)
 mf1 <- c(0.1, 0.3, 0.7, 0.9, 1.0, 0.8, 0.6, 0.2, 0.3, 0.9, 0.4)
 N <- matrix(mf1, nrow=4, ncol=3, byrow=TRUE)
@@ -17,6 +18,5 @@ for(i in r1)
   for (j in r2)
   {
     mul[i][j]<-0
-    for(clr2)
   }
 }
