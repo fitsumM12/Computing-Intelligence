0a524aa0a97ecbcb3fb54cecd5de6abd4529e674
fitsumM12 <fitsummesfindejene@gmail.com>
2022-04-27 11:51:19 +0530

    update

--- WEEK 06/Q2.R
+++ WEEK 06/Q2.R
@@ -1,10 +1,10 @@
 library(FuzzyR)
 library(sets)
-# Find De-fuzzified values
-# Centroid
+# Bisector of Area
 
 mf1<-genmf('trapmf', c(10,20,40,70))
 trap<-evalmf(0:100, mf1)
 plot(trap, type="l",main="Trapezoidal MF",lwd=3)
-crisp_val <-defuzz(0:100,trap,"centroid")
+crisp_val <-defuzz(0:100,trap,"bisector")
 print(crisp_val)
+abline(v=crisp_val, col="red",lty=2,lwd=3)
