c9ad131a9b5c80836e34b2c0942d71b36b62168f
fitsumM12 <fitsummesfindejene@gmail.com>
2022-05-06 13:53:52 +0530

    update

--- WEEK  07/Q1.R
+++ WEEK  07/Q1.R
@@ -8,19 +8,20 @@ underwritting rating.
 library(FuzzyR)
 library(sets)
 library(ggplot2)
-sets_options("universe",seq(0, 40, 0.1))
+
+sets_options("universe", seq(from = 0, to = 40, by = 0.1))
 variables <- set(
   bmi = fuzzy_partition(varnames = c(under = 9.25, fit = 21.75, over = 27.5, obese = 35),
-                      sd = 3.0),
+                        sd = 3.0),
   a1c = fuzzy_partition(varnames = c(l = 4, n = 5.25, h = 7),
-                      FUN = fuzzy_cone, radius = 5),
+                        FUN = fuzzy_cone, radius = 5),
   bp = fuzzy_partition(varnames = c(norm = 0, pre = 10, hyp = 20, shyp = 30),
                        sd = 2.5),
   rating = fuzzy_partition(varnames = c(DC=10, ST=5, PF=1),
                            FUN = fuzzy_cone, radius = 5)
 )
 rules <- set(
-  fuzzy_rule(bmi %is% under || bmi %is% obese || a1c %is% 1,
+  fuzzy_rule(bmi %is% under || bmi %is% obese || a1c %is% l,
              rating %is% DC),
   fuzzy_rule(bmi %is% over || a1c %is% n || bp %is% pre, 
              rating %is% ST),
@@ -31,10 +32,10 @@ system<- fuzzy_system(variables, rules)
 print(system)
 plot(system)
 
-fi <- fuzzy_inference(system, list(bmi=29, a1c==5, bp=20) )
+fi <- fuzzy_inference(system, list(bmi=29, a1c=5, bp=20))
 plot(fi)
 
-x_val = gset_defuzzify(fi, method="centroid")
+x_val = gset_defuzzify(fi, "centroid")
 y_val = fi[round(x_val)]
-lines(x_val, fi[x_val], type = 'h', lty =2, col = "red")
+lines(x_val, y_val, type = 'h', lty =2, col = "red")
 axis(1, at = x_val, las = 2)
