6152e243e625250b52e36e728d814f78bbc5d4d4
fitsumM12 <fitsummesfindejene@gmail.com>
2022-05-18 13:05:25 +0530

    update

--- WEEK 09/exercise 02.R
+++ WEEK 09/exercise 02.R
@@ -5,16 +5,26 @@ library(neuralnet)
 
 ## creating training data set
 traininginput =as.data.frame(runif(50, min=0, max=225))
+traininginput
 trainingoutput = sqrt(traininginput)
-trainingdata = cbind(traininginput, trainingoutput)
+trainingoutput
+trainingdata = data.frame(traininginput, trainingoutput)
 colnames(trainingdata) = c(Input,Output)
+
 print(trainingdata)
+
+## creating the neural network
+nn_1= neuralnet(trainingoutput~traininginput , data = trainingdata,
+                hidden = 10, threshold = 0.01, lifesign = "full", lifesign.step = 10,
+)
+print(nn_1)
+plot(nn_1)
+
 
 ## creating training dataset
 train_in = c(0, 1, 4,9,16,25, 36, 49, 64, 81, 100, 121, 144, 169, 196,225)
 train_out = c(0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 train_data = data.frame(train_in,train_out)
-
 
 
 
